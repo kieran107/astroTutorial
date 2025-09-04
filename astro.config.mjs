49dd62e9ffd2860f1a58d157ca9b42431c6e55cf
k1eran <71857504+kieran107@users.noreply.github.com>
2025-09-04 15:56:50 +0800

    island finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://rainbow-lollipop-cff8b1.netlify.app"
+  site: "https://rainbow-lollipop-cff8b1.netlify.app",
+  integrations: [preact()]
 });
